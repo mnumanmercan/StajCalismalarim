50fa53747e723cb9ba11343587b75ed5809aced0
MNMercan <numan.mercan24@gmail.com>
2022-08-22 16:13:38 +0300

    first commit

---
+++ ToDo App/script.js
@@ -0,0 +1,23 @@
+var addBtn = document.querySelector("#add");
+var task = document.querySelector("#addTask")
+var taskDiv = document.querySelector(".tasks");
+
+addBtn.addEventListener("click", () => {
+
+    if (task.value.length == 0) {
+        alert("Enter Task please!")
+    } else {
+        console.log(task.value)
+        var li = document.createElement("li");
+        var input = task.value;
+        var t = document.createTextNode(input);
+        li.style.listStyleType = "upper-roman";
+        li.append(t);
+        taskDiv.append(li);
+        console.log(t);
+        console.log(li);
+
+        task.value = "";
+    }
+
+});
